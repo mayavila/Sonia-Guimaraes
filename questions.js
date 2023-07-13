561eadbd5f3b0bf986381bb00a1df9a39b093888
[mayavila] <[maycris_05@hotmail.com]>
2023-07-12 23:34:56 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,50 @@
+[
+    {
+      question: "Em qual área do conhecimento Sonia Guimarães obteve seu doutorado?",
+      answers: [
+        { option: "Química", correct: false },
+        { option: "Física", correct: true },
+        { option: "Biologia", correct: false },
+      ],
+    },
+    {
+      question: "Qual a Instituição de ensino Sonia Guimarães se tornou a primeira mulher negra a ser professora?",
+      answers: [
+        { option: "Universidade de São Paulo (USP)", correct: false },
+        { option: "Universidade Federal do Rio de Janeiro (UFRJ)", correct: false },
+        { option: "Instituto Tecnológico de Aeronáutica (ITA)", correct: true },
+      ],
+    },
+    {
+      question: "Qual é o objetivo da Universidade Zumbi dos Palmares, Instituição que Sonia é mantenedora?",
+      answers: [
+        { option: "Promover a inclusão e a igualdade racial na educação superior", correct: true },
+        { option: "Fomentar pesquisas na área de tecnologia", correct: false },
+        { option: "Formar profissionais na área de ciências humanas", correct: false },
+      ],
+    },
+    {
+      question: "Em qual curso Sonia Guimarães ministra mentoria para estudantes negras?",
+      answers: [
+        { option: "Medicina", correct: false },
+        { option: "Engenharia", correct: false },
+        { option: "Ciência da Computação", correct: true },
+      ],
+    },
+    {
+    question: "Qual é a área de especialização de Sonia Guimarães??",
+    answers: [
+        { option: "Inteligência Artificial", correct: false },
+        { option: "Semicondutores", correct: true },
+        { option: "Energia Renonável", correct: false },
+      ],
+    },
+    {
+      question: "Em que ano Sonia Guimarães se tornou a primeira mulher negra a dar aulas no Departamento de Física do ITA?",
+      answers: [
+        { option: "1980", correct: false },
+        { option: "1993", correct: true },
+        { option: "2005", correct: false },
+      ],
+    },
+];
